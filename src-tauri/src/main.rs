a673b449aa0dd2b5230391fd15283aeb0f8c6be5
Domenic Walther <domenicwalthercreatiiv@gmail.com>
2024-02-25 19:20:20 +0100

    feat(dev): remove arboard

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,7 +1,6 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use arboard::Clipboard;
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
 fn greet(name: &str) -> String {
@@ -9,8 +8,6 @@ fn greet(name: &str) -> String {
 }
 
 fn main() {
-    let mut clipboard = Clipboard::new().unwrap();
-    println!("Clipboard content: {}", clipboard.get_text().unwrap());
     tauri::Builder::default()
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
