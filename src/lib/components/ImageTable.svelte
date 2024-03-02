43fae38b8e43baa107ffc0a06944d38d2fc6ef35
Domenic Walther <domenicwalthercreatiiv@gmail.com>
2024-03-02 23:19:45 +0100

    fix: language issue, remove console.log

--- src/lib/components/ImageTable.svelte
+++ src/lib/components/ImageTable.svelte
@@ -1,59 +1,58 @@
 <script lang="ts">
-    import * as Table from "$lib/components/ui/table";
-    import { imageList } from "$lib/store";
-    import type { orderType } from "$lib/types";
-    const deleteRow = (deleteIndex: number) => {
-        console.log("Deleting!");
-        imageList.update((currentItems) =>
-            currentItems.filter((_: orderType, i: number) => i !== deleteIndex),
-        );
-    };
+	import * as Table from '$lib/components/ui/table';
+	import { imageList } from '$lib/store';
+	import type { orderType } from '$lib/types';
+	const deleteRow = (deleteIndex: number) => {
+		imageList.update(currentItems =>
+			currentItems.filter((_: orderType, i: number) => i !== deleteIndex)
+		);
+	};
 </script>
 
 <Table.Root>
-    <Table.Caption>A list of your recent invoices.</Table.Caption>
-    <Table.Header>
-        <Table.Row>
-            <Table.Head class="w-[100px]">Bildnummer</Table.Head>
-            <Table.Head class="w-[100px]">Größe</Table.Head>
-            <Table.Head>Farbe</Table.Head>
-            <Table.Head>Anzahl</Table.Head>
-            <Table.Head>Beschreibung</Table.Head>
-            <Table.Head class="text-right">Amount</Table.Head>
-            <Table.Head class="text-right">Actions</Table.Head>
-        </Table.Row>
-    </Table.Header>
-    <Table.Body>
-        {#each $imageList as image, i (i)}
-            <Table.Row>
-                <Table.Cell class="font-medium"
-                    ><input type="text" value={image.imageNumber} /></Table.Cell
-                >
-                <Table.Cell class="font-medium"
-                    ><select class="bg-white rounded-none"
-                        ><option>13x18cm</option><option>20x30cm</option
-                        ></select
-                    ></Table.Cell
-                >
-                <Table.Cell
-                    ><select
-                        ><option>Farbe</option><option>Mischton</option><option
-                            >Braunton</option
-                        ><option>SchwarzWeiß</option></select
-                    ></Table.Cell
-                >
-                <Table.Cell
-                    ><input type="text" value={image.amount} /></Table.Cell
-                >
-                <Table.Cell
-                    ><input type="text" value={image.description} /></Table.Cell
-                >
-                <Table.Cell class="text-right">{image.price}€</Table.Cell>
-                <Table.Cell class="text-right"
-                    ><button on:click={() => deleteRow(i)}>Löschen</button
-                    ></Table.Cell
-                >
-            </Table.Row>
-        {/each}
-    </Table.Body>
+	<Table.Caption>A list of your recent invoices.</Table.Caption>
+	<Table.Header>
+		<Table.Row>
+			<Table.Head class="w-[100px]">Bildnummer</Table.Head>
+			<Table.Head class="w-[100px]">Größe</Table.Head>
+			<Table.Head>Farbe</Table.Head>
+			<Table.Head>Anzahl</Table.Head>
+			<Table.Head>Beschreibung</Table.Head>
+			<Table.Head class="text-right">Preis</Table.Head>
+			<Table.Head class="text-right">Actions</Table.Head>
+		</Table.Row>
+	</Table.Header>
+	<Table.Body>
+		{#each $imageList as image, i (i)}
+			<Table.Row>
+				<Table.Cell class="font-medium">
+					<input type="text" value={image.imageNumber} />
+				</Table.Cell>
+				<Table.Cell class="font-medium">
+					<select class="bg-white rounded-none">
+						<option>13x18cm</option>
+						<option>20x30cm</option>
+					</select>
+				</Table.Cell>
+				<Table.Cell>
+					<select>
+						<option>Farbe</option>
+						<option>Mischton</option>
+						<option>Braunton</option>
+						<option>SchwarzWeiß</option>
+					</select>
+				</Table.Cell>
+				<Table.Cell>
+					<input type="text" value={image.amount} />
+				</Table.Cell>
+				<Table.Cell>
+					<input type="text" value={image.description} />
+				</Table.Cell>
+				<Table.Cell class="text-right">{image.price}€</Table.Cell>
+				<Table.Cell class="text-right">
+					<button on:click={() => deleteRow(i)}>Löschen</button>
+				</Table.Cell>
+			</Table.Row>
+		{/each}
+	</Table.Body>
 </Table.Root>
